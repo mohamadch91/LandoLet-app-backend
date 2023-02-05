439057fcd7029684691e7b47e7fd16d858eaf040
mohamadch91 <mohamadchoupan80@gmail.com>
2023-02-06 02:15:24 +0330

    feat: generate pdf for each property

--- getprops/urls.py
+++ getprops/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('propsview/', propsview.as_view(), name='get_prop'),
     path('propertydetail/', propertydetail.as_view(), name='get_prop_detail'),
     path('send_to_tenant/', sendtoTenantView.as_view(), name='get_prop_detail'),
+    path('generatePDF/<int:id>/', generatePDF.as_view(), name='get_prop_detail'),
     
 
 ]
