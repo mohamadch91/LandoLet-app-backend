8c55c59e9f818c609d23f4eda07e99ec9ed20201
mohamadch91 <mohamadchoupan80@gmail.com>
2023-02-06 01:45:30 +0330

    feat: add db_index to authen

--- authen/models.py
+++ authen/models.py
@@ -45,16 +45,16 @@ class User(AbstractUser):
         LANLORD="Landlord"    
         AGENCY="Agency"
     username=None    
-    id = models.AutoField(db_column='Id', primary_key=True)  # Field name made lowercase.
+    id = models.AutoField(db_column='Id', primary_key=True,db_index=True)  # Field name made lowercase.
    # Field name made lowercase.
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
  # Field name made lowercase.
-    role=models.CharField(db_column='RoleName',max_length=10,choices=Role.choices, null=True)
+    role=models.CharField(db_column='RoleName',max_length=10,choices=Role.choices, null=True,db_index=True)
     # username = models.TextField(db_column='Username', blank=True, null=True)  # Field name made lowercase.
     first_name = models.TextField(db_column='FirstName', blank=True, null=True)  # Field name made lowercase.
     last_name = models.TextField(db_column='LastName', blank=True, null=True)  # Field name made lowercase.
-    email = models.EmailField(db_column='Email',unique=True, blank=True, null=True)   # Field name made lowercase.
+    email = models.EmailField(db_column='Email',unique=True, blank=True, null=True,db_index=True)   # Field name made lowercase.
     postal_code = models.IntegerField(db_column='PostalCode', blank=True, null=True)  # Field name made lowercase.
     full_address = models.TextField(db_column='FullAddress', blank=True, null=True)  # Field name made lowercase.
     phone_no = models.IntegerField(db_column='PhoneNo', blank=True, null=True)  # Field name made lowercase.
