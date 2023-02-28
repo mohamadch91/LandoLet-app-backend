d61afda90189f943480a2990750baf877c5de5cf
mohamadch91 <mohamadchoupan80@gmail.com>
2023-02-28 10:26:43 +0330

    fix: view in url

--- getprops/urls.py
+++ getprops/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('propsview/', PropsView.as_view(), name='get_prop'),
     path('propertydetail/', PropertyDetail.as_view(), name='get_prop_detail'),
     path('send_to_tenant/', SendtoTenantView.as_view(), name='send to tenant '),
-    path('send_to_landolet/', SendtoTenantView.as_view(), name='send to owner'),
+    path('send_to_landolet/', SendtoOwner.as_view(), name='send to owner'),
     path('generatePDF/<int:id>/', GeneratePDF.as_view(), name='get_prop_pdf'),
     
     
