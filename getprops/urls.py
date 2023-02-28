b89fb29d07d379570a154aee8154df4d424b707d
mohamadch91 <mohamadchoupan80@gmail.com>
2023-02-28 09:53:17 +0330

    feat: add new APi to urls

--- getprops/urls.py
+++ getprops/urls.py
@@ -5,7 +5,8 @@ from .views import *
 urlpatterns = [
     path('propsview/', PropsView.as_view(), name='get_prop'),
     path('propertydetail/', PropertyDetail.as_view(), name='get_prop_detail'),
-    path('send_to_tenant/', SendtoTenantView.as_view(), name='get_prop_detail'),
+    path('send_to_tenant/', SendtoTenantView.as_view(), name='send to tenant '),
+    path('send_to_landolet/', SendtoTenantView.as_view(), name='send to owner'),
     path('generatePDF/<int:id>/', GeneratePDF.as_view(), name='get_prop_pdf'),
     
     
